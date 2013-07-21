6d82a7756abf3b1e3062d9e7c720f8c9cdb0176f
Matteo Landi <matteo@matteolandi.net>
2013-07-21 04:32:36 +0200

    Something's rotten in the state of Denmark. 		-- Shakespeare

--- app/tasks.py
+++ app/tasks.py
@@ -39,29 +39,42 @@ def process_comment(access_token, party_id, post, pending):
 
 @celery.task
 def PollPartyTask(user, since=0):
+    access_token = user.token
+    party_id = user.party_id
+    while True:
+        session = create_session()
+        resp = json.load(
+                urllib.urlopen(
+                    'https://graph.facebok.com/' + party_id + '/feed?' +
+                    urllib.urlencode(dict(fields='comments,message,from',
+                                        date_format='U',
+                                        since=since,
+                                        access_token=access_token))))
+        posts = [p for p in resp['data']]
+        comments = [p for p in posts if 'message' in p]
+
+        pending = len(RequestsRepository.pending())
+        for c in comments:
+            r = process_comment(access_token, party_id, c, pending)
+            if r is not None:
+                session.add(r)
+                MakeBirroTask.apply(access_token, r)
+                pending += 1
+        session.commit()
+
+        # Reschedule next execution
+        time.sleep(5)
+        since = posts[0]['created_time'] if posts else since
+
+
+@celery.task
+def MakeBirroTask(access_token, request):
     session = create_session()
-    #access_token = self.current_user.token XXX
-    access_token = 'CAAFgztesdO8BAMSIUjvmW0aewKp1ZBHPEtT95Rfe1it1DMTR1OzORZB9LOusFVHWx1aHStBMv7cwBwEf2cHPAuU2b4BNywJQsm0gTEUZC21jvkxiO3L4Tr07HcbkNIZCBIS7J3DUBh3RF6E7R2spyuwisTEXPSAZD'
-    #party_id = self.current_user.party_id XXX
-    party_id = '417578625021950'
     resp = json.load(
             urllib.urlopen(
-                'https://graph.facebok.com/' + party_id + '/feed?' +
-                urllib.urlencode(dict(fields='comments,message,from',
-                                      date_format='U',
-                                      since=since,
-                                      access_token=access_token))))
-    posts = [p for p in resp['data']]
-    comments = [p for p in posts if 'message' in p]
-
-    pending = len(RequestsRepository.pending())
-    for c in comments:
-        r = process_comment(access_token, party_id, c, pending)
-        if r is not None:
-            session.add(r)
-            pending += 1
+                'https://graph.facebok.com/' + request.comment_id + '/comments',
+                urllib.urlencode(dict(message='Come here!',
+                    access_token=access_token))))
+    request.served = True
+    session.add(request)
     session.commit()
-
-    # Reschedule next execution
-    time.sleep(5)
-    PollPartyTask.delay(user, posts[0]['created_time'] if posts else since)
