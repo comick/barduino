5b70bf687f061658d7b91c3fa66b50152e1738b2
Matteo Landi <matteo@matteolandi.net>
2013-07-22 17:33:20 +0200

    The better part of valor is discretion. 		-- William Shakespeare, "Henry IV"

--- celeryconfig.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from celery.schedules import crontab
-
-CELERY_IMPORTS = ('app.tasks',)
-
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
